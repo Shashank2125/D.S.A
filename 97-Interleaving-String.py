10da4f071301cf5716bf53adb564e361108280c7
Shashank Shekhar Singh <138694950+Shashank2125@users.noreply.github.com>
2026-08-06 13:44:53 +0530

    [97] [Time Beats: 50.88%] [Memory Beats: 23.28%] - LeetPush

---
+++ 97-Interleaving-String.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        n=len(s1)
+        m=len(s2)
+        if n+m!=len(s3):
+            return False
+        dp=[[False]*(m+1)for _ in range(n+1)]
+        dp[0][0]=True
+        for i in range(1,n+1):
+            dp[i][0]=dp[i-1][0] and (s1[i-1]==s3[i-1])
+        for j in range(1,m+1):
+            dp[0][j]=dp[0][j-1] and(s2[j-1]==s3[j-1])
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s1[i-1]==s3[i+j-1]:
+                    dp[i][j]=dp[i-1][j]or dp[i][j]
+                if s2[j-1]==s3[i+j-1]:
+                    dp[i][j]=dp[i][j-1] or dp[i][j]
+        return dp[n][m]
+        
+        
