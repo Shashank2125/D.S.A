c74d333d352100d802b34ad6855aa16ba419484b
Shashank Shekhar Singh <138694950+Shashank2125@users.noreply.github.com>
2026-08-24 15:53:23 +0530

    [11] [Time Beats: 6.33%] [Memory Beats: 64.20%] - LeetPush

---
+++ 11-Container-With-Most-Water.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        max_area=float("-inf")
+        i=0
+        j=len(height)-1
+        while i<j:
+            area=min(height[i],height[j])*(j-i)
+            if height[i]<height[j]:
+                i+=1
+            else:
+                j-=1
+            max_area=max(max_area,area)
+        return max_area
+
+
+        
